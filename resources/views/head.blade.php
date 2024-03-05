42950d79ff3770f0ce0a01c7b830f3fdf6f447c6
Văn Hiệp <dhiep2307@gmail.com>
2024-03-06 04:10:53 +0700

    fig nav mobile

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 
 <link rel="stylesheet" href="/assets/css/{{$cssVersion}}/index.css" />
+<link rel="stylesheet" href="/assets/css/{{$cssVersion}}/nav-mobile.css" />
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons">
@@ -29,8 +30,8 @@
 <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
-@if(isset($title))
-    <title>{{$title}} | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh</title>
+@if(isset($seo))
+    <title>{{$seo['title']}}</title>
 @else
     <title>Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh</title>
 @endif
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ $router->get('/', function () use ($router) {
         'title' => 'Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Chuyên phục vụ ăn uống, ẩm thực các món đặc trưng 3 miền. Nhận đặt tiệc liên hoan, sinh nhật,... Cho thuê phòng nghỉ cao cấp rộng rãi, thoáng mát, đầy đủ tiện nghi.',
         'url' => env('APP_URL'),
-        'image' => env('APP_URL') . '/thumb/home.png'
+        'image' => env('APP_URL') . '/thumb/home-min.png'
     ];
     $data = ['activePage' => 0, 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.home', $data);
@@ -29,7 +29,7 @@ $router->get('/khu-hoi-truong-to-chuc-tiec', function () use ($router) {
         'title' => 'Hội Trường tổ chức tiệc | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Chuyên phục vụ ăn uống, ẩm thực các món đặc trưng 3 miền. Nhận đặt tiệc liên hoan, sinh nhật,... Cho thuê phòng nghỉ cao cấp rộng rãi, thoáng mát, đầy đủ tiện nghi.',
         'url' => env('APP_URL') . '/khu-hoi-truong-to-chuc-tiec',
-        'image' => env('APP_URL') . '/thumb/home.png'
+        'image' => env('APP_URL') . '/thumb/home-min.png'
     ];
     $data = ['activePage' => 1, 'title' => 'Khu tiệc, hội trường', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.restaurant.hall', $data);
@@ -40,7 +40,7 @@ $router->get('/khu-phong-lanh', function () use ($router) {
         'title' => 'Phòng lạnh | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Chuyên phục vụ ăn uống, ẩm thực các món đặc trưng 3 miền. Nhận đặt tiệc liên hoan, sinh nhật,... Cho thuê phòng nghỉ cao cấp rộng rãi, thoáng mát, đầy đủ tiện nghi.',
         'url' => env('APP_URL') . '/khu-phong-lanh',
-        'image' => env('APP_URL') . '/thumb/home.png'
+        'image' => env('APP_URL') . '/thumb/home-min.png'
     ];
     $data = ['activePage' => 2, 'title' => 'Khu phòng ăn phòng lạnh', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.restaurant.cold-room', $data);
@@ -51,7 +51,7 @@ $router->get('/khu-san-vuon', function () use ($router) {
         'title' => 'Sân vườn | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Sân vườn rộng rãi, sạch sẽ, thoáng gió. Các Tum mái lá mát mẻ, cảm giác hòa mình với thiên nhiên. Chuyên phục vụ ăn uống, ẩm thực các món đặc trưng 3 miền. Nhận đặt tiệc liên hoan, sinh nhật,... Cho thuê phòng nghỉ cao cấp rộng rãi, thoáng mát, đầy đủ tiện nghi.',
         'url' => env('APP_URL') . '/khu-san-vuon',
-        'image' => env('APP_URL') . '/thumb/home.png'
+        'image' => env('APP_URL') . '/thumb/home-min.png'
     ];
     $data = ['activePage' => 3, 'title' => 'Khu bàn ăn sân vườn', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.restaurant.garden', $data);
@@ -62,7 +62,7 @@ $router->get('/mon-an-noi-bat', function () use ($router) {
         'title' => 'Thực đơn, món ăn, ẩm thực | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Chuyên phục vụ các món ăn, ẩm thực 3 miền. Với đầy đủ các nguyên vật liệu và cách chế biến. Nhân viên phục vụ nhiệt tình, chu đáo.',
         'url' => env('APP_URL') . '/mon-an-noi-bat',
-        'image' => env('APP_URL') . '/thumb/food.png'
+        'image' => env('APP_URL') . '/thumb/food-min.png'
     ];
     $data = ['activePage' => 4, 'title' => 'Món ăn nổi bật', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.restaurant.menu', $data);
@@ -73,7 +73,7 @@ $router->get('/nha-nghi', function () use ($router) {
         'title' => 'Nhà nghỉ Khách sạn Trà Vinh | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Nhà nghỉ của chúng tôi mang đến một không gian ấm cúng và tiện nghi. Được trang bị đầy đủ tiện ích hiện đại du khách có thể thưởng thức kỳ nghỉ của mình.',
         'url' => env('APP_URL') . '/nha-nghi',
-        'image' => env('APP_URL') . '/thumb/motel.png'
+        'image' => env('APP_URL') . '/thumb/motel-min.png'
     ];
     $data = ['activePage' => 5, 'title' => 'Nhà nghỉ khách sạn', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.motel.index', $data);
@@ -84,7 +84,7 @@ $router->get('/lien-he-chung-toi', function () use ($router) {
         'title' => 'Liên hệ chúng tôi | Nhà hàng Khách sạn Hương Rừng HomeStay, Nhà hàng Trà Vinh, Khách sạn Trà Vinh',
         'description' => 'Nhà nghỉ của chúng tôi mang đến một không gian ấm cúng và tiện nghi. Nhà hàng chuyên phục vụ ăn uống, ẩm thực các món đặc trưng 3 miền. Nhận đặt tiệc liên hoan, sinh nhật,... Phòng nghỉ cao cấp rộng rãi, thoáng mát, đầy đủ tiện nghi.',
         'url' => env('APP_URL') . '/lien-he-chung-toi',
-        'image' => env('APP_URL') . '/thumb/home.png'
+        'image' => env('APP_URL') . '/thumb/home-min.png'
     ];
     $data = ['activePage' => 6, 'title' => 'Liên hệ chúng tôi, đặt phòng, đặt tiệc', 'cssVersion' => env('CSS_VERSION'), 'seo' => $metaTag];
     return view('pages.contact.index', $data);
